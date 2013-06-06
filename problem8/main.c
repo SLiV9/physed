17487c2b919b006b548e73e97b1924e145a82411
SLiV <sanderintveld@hotmail.com>
2013-06-06 13:21:23 +0200

    [8] klaar om te beginnen

--- problem8/main.c
+++ problem8/main.c
@@ -2,9 +2,9 @@
 
 int main(int argc, char** argv)
 {
-    printf("[ Project Euler: Problem 7 ]\n\n\n");
+    printf("[ Project Euler: Problem 8 ]\n\n\n");
     
-    printf("The 10001th prime is %d.\n", 2);
+    printf("The answer is 1.\n");
 
     printf("\n\n[ done ]\n");
 
