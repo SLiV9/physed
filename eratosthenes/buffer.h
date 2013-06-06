355e88ed66855b41f7668b0a528b6528c0d3de51
SLiV <sanderintveld@hotmail.com>
2013-06-06 14:30:09 +0200

    [10] te langzaam en buffer tradeoff is in beide kanten onvolledig

--- eratosthenes/buffer.h
+++ eratosthenes/buffer.h
@@ -1,4 +1,4 @@
-#define BUFFERSIZE (1<<4)
+#define BUFFERSIZE (1<<16)
 
 /* A FIFO buffer for storing natural (i.e. positive whole) numbers. */
 typedef struct buffer
--- eratosthenes/main.c
+++ eratosthenes/main.c
@@ -3,7 +3,7 @@
 
 #include "sieve.h"
 
-#define PRIME_INDEX 10001
+#define PRIME_INDEX 1000000
 
 /* The main thread method. */
 int main(int argc, char** argv)
@@ -11,15 +11,22 @@ int main(int argc, char** argv)
   printf("[ Eratosthenes Sieve ]\n\n\n");
   
   unsigned long last_prime;
+  unsigned long long total = 2;
   
   init_sieve();
   
-  printf("Primes:\n");
+  printf("Primes: \t2\n");
   start();
   for (int i = 1; i < PRIME_INDEX; i++)
   {
     while ((last_prime = get_prime()) == BUSY);
+    if (last_prime >= 2000000)
+    {
+      break;
+    }
+    
     printf(" %lu \t", last_prime);
+    total += last_prime;
     advance();
   }
   end();
@@ -27,7 +34,7 @@ int main(int argc, char** argv)
   
   free_sieve();
   
-  printf("The 10th prime is %lu.\n", last_prime);
+  printf("The sum of primes below 2000000 is %llu.\n", total);
   
   printf("\n\n[ done ]\n");
 }
