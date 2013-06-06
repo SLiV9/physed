f67f53b9b17e27413f609b370b228a8ba60466dd
SLiV <sanderintveld@hotmail.com>
2013-06-06 13:40:26 +0200

    [8] opgelost

--- problem8/main.c
+++ problem8/main.c
@@ -1,10 +1,49 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv)
 {
     printf("[ Project Euler: Problem 8 ]\n\n\n");
     
-    printf("The answer is 1.\n");
+    char digit[1000];
+    char bin[1];
+    
+    FILE* fdigits = fopen("digits.txt", "r");
+    for (int i = 0; i < 20; i++)
+    {
+      fread(&digit[50 * i], sizeof(char), 50, fdigits);
+      fread(&bin, sizeof(char), 1, fdigits);
+    }
+    
+    for (int i = 0; i < 1000; i++)
+    {
+      digit[i] = digit[i] - '0';
+      printf("%d", (unsigned int) digit[i]);
+      if ((i+1) % 50 == 0)
+        printf("\n");
+    }
+    printf("\n");
+    
+    unsigned long current, greatest = 0;
+    for (int i = 0; i < 996; i++)
+    {
+      if (digit[i] && digit[i+1] && digit[i+2] && digit[i+3] && digit[i+4])
+      {
+        if ((current = ((unsigned long) digit[i]) * digit[i+1] * digit[i+2] 
+            * digit[i+3] * digit[i+4]))
+        {
+          if (current > greatest)
+          {
+            greatest = current;
+          }
+        }
+      }
+    }
+    
+    printf("The greatest product of 5 consecutive digits is %lu.\n", greatest);
+    
+    fclose(fdigits);
 
     printf("\n\n[ done ]\n");
 
