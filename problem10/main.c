fa926052a68a793285ee97a115cc2e6b41185406
SLiV <sanderintveld@hotmail.com>
2013-06-07 12:07:45 +0200

    [10] opgelost in 10.3 seconden

--- problem10/main.c
+++ problem10/main.c
@@ -4,16 +4,20 @@
 
 #include "bitmap.h"
 
+#define CLEAN 0
+#define NOTPRIME 1
+
 #define VAL(D) (2*D + 3)
 #define ID(V) ((V-3) / 2)
 
 typedef unsigned long natural;
 
+/* Find the next prime number by finding the next non-dirty bit. */
 natural get_next_prime(unsigned int d)
 {
-  for (unsigned int u = d; u < ID(1999999); u++)
+  for (unsigned int u = d; u <= ID(1999999); u++)
   {
-    if (!getbit(u))
+    if (getbit(u) != NOTPRIME)
     {
       return VAL(u);
     }
@@ -22,6 +26,19 @@ natural get_next_prime(unsigned int d)
   return 0;
 }
 
+/* We want to mark multiples of p as dirty: i.e. setbit(2p), setbit(3p), ...
+ * However, 2p % 2 == 0, so we only need 3p, 5p, 7p, 9p, ...
+ * ID((2k+1)p) = ((2kp+p-3)/2) = kp + ((p-3)/2) = kp + ID(p), for k = 1, 2, ...
+ * This is how we find the following for-loop. */
+void filter(natural p)
+{
+  for (unsigned int u = ID(p) + p; u <= ID(1999999); u += p)
+  {
+    setbit(u, NOTPRIME);
+  }
+}
+
+/* Our main method. */
 int main(int argc, char** argv)
 {
     printf("[ Project Euler: Problem 10 ]\n\n\n");
@@ -35,6 +52,7 @@ int main(int argc, char** argv)
     {
       total += next_prime;
       printf("%lu \t", next_prime);
+      filter(next_prime);
       prime_index = ID(next_prime) + 1;
     }
     printf("end.\n\n");
