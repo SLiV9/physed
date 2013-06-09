b93096516ccf625a86dfd1fcb8c8cd777b6369a3
SLiV <sanderintveld@hotmail.com>
2013-06-09 18:31:23 +0200

    [13] opgelost

--- main.c
+++ main.c
@@ -2,14 +2,17 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define DIGIT(i, t) data[50 * (i) + (t)]
+#define DIGIT(i, t) data[50 * (i) + 49 - (t)]
+#define SUMDIGIT(t) sum[51 - (t)]
 
 int main(int argc, char** argv)
 {
     printf("[ Project Euler: Problem 13 ]\n\n\n");
     
     char data[5000];
+    char sum[52];
     char bin;
+    memset(sum, 0, 52);
     
     FILE* fdigits = fopen("data/problem13/numbers.txt", "r");
     if (!fdigits)
@@ -35,13 +38,33 @@ int main(int argc, char** argv)
     {
       for (int t = 0; t < 50; t++)
       {
-        printf("%d", (int) DIGIT(i, t));
+        printf("%d", (int) DIGIT(i, 50 - t));
       }
       printf("\n");
     }
     printf("< end of data >\n\n");
     
-    printf("The answer is unknown.\n");
+    /* Starting at the least significant bit first, we add all the digits. */
+    int w;
+    for (int t = 0; t < 50; t++)
+    {
+      w = 0;
+      w += SUMDIGIT(t);
+      for (int i = 0; i < 100; i++)
+      {
+        w += DIGIT(i, t);
+      }
+      SUMDIGIT(t) = (char) (w % 10);
+      SUMDIGIT(t+1) += (char) ((w / 10) % 10);
+      SUMDIGIT(t+2) += (char) (w / 100);
+    }
+    
+    printf("The answer is: ");
+    for (int t = 0; t < 52; t++)
+    {
+      printf("%d", (int) sum[t]);
+    }
+    printf(".\n");
     
     printf("\n\n[ done ]\n");
 
