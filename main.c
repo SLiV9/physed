54ecae09dcc394833dd00b3014111c2988ef8451
SLiV <sanderintveld@hotmail.com>
2013-06-09 17:47:48 +0200

    [11] data ingeladen

--- main.c
+++ main.c
@@ -4,45 +4,29 @@
 
 int main(int argc, char** argv)
 {
-    printf("[ Project Euler: Problem 12 ]\n\n\n");
+    printf("[ Project Euler: Problem 11 ]\n\n\n");
     
-    unsigned long w = 0;
-    unsigned int divs = 0;
+    char buffer[4] = "\0\0\0\0";
+    int data[400];
     
-    for (unsigned long x = 1; x < 999999; x++)
+    FILE* fdigits = fopen("data/problem11/data.txt", "r");
+    if (!fdigits)
     {
-      w += x;
-      
-      unsigned long n;
-      
-      /* Now to calculate the number of divisors. 
-       * If n | w == 0, then w = n * k for some k in Z. However, note that 
-       * w = k * n and therefore k | w, so each divisor has a counterpart;
-       * apart from sqrt(w). Also, not that if n < sqrt(w), then k > sqrt(w).
-       * We only need to test the numbers that are smaller than sqrt(w), which, 
-       * for w >= 1000000 is less than 0.1% of the numbers below w. */
-      divs = 0;
-      for (n = 1; n * n < w; n++)
-      {
-        if (w % n == 0)
-        {
-          divs += 2;
-        }
-      }
-      if (n * n == w)
-      {
-        divs += 1;
-      }
-      
-      printf("%lu: %u\n", w, divs);
-      
-      if (divs >= 500)
-      {
-        break;
-      }
+      perror("fopen error");
+      return -1;
     }
     
-    printf("Answer: w = %lu.\n", w);
+    printf("Input:\n");
+    /* The file consists of 400 lines, seperated by either a space or a newline 
+     * character. */
+    for (int i = 0; i < 400; i++)
+    {
+      fread(buffer, sizeof(char), 3, fdigits);
+      printf("%s", buffer);
+    }
+    printf("< end of file >\n\n");
+    
+    printf("The answer is unknown.\n");
     
     printf("\n\n[ done ]\n");
 
